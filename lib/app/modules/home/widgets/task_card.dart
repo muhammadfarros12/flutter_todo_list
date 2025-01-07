0079f23db7f255c30911ff019ab008157203d5e9
ammaar12 <muhammadfarroos12@gmail.com>
2025-01-07 16:45:07 +0700

    Gradient progress indicator & style task card

--- lib/app/modules/home/widgets/task_card.dart
+++ lib/app/modules/home/widgets/task_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:todo_list/app/core/utils/extensions.dart';
 
 import 'package:todo_list/app/data/models/task.dart';
@@ -9,9 +10,9 @@ class TaskCard extends StatelessWidget {
   final homeController = Get.find<HomeController>();
   final Task task;
   TaskCard({
-    Key? key,
+    super.key,
     required this.task,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -27,10 +28,58 @@ class TaskCard extends StatelessWidget {
         BoxShadow(
           color: Colors.grey[300]!,
           blurRadius: 7,
-          offset: Offset(0, 7),
+          offset: const Offset(0, 7),
         ),
       ]),
-      child: Column(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          StepProgressIndicator(
+            // TODO change after finished todo CRUD
+            totalSteps: 100,
+            currentStep: 80,
+            size: 5,
+            padding: 0,
+            selectedGradientColor: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [color.withOpacity(0.5), color]),
+            unselectedGradientColor: const LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.white, Colors.white]),
+          ),
+          Padding(
+            padding: EdgeInsets.all(6.0.wp),
+            child: Icon(
+              IconData(task.icon, fontFamily: 'MaterialIcons'),
+              color: color,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(6.0.wp),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  task.title,
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 12.0.sp),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(
+                  height: 1.0.wp,
+                ),
+                Text(
+                  '${task.todos?.length ?? 0} Task',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.grey),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
